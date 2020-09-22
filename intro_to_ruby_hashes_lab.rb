5f43a202da564d521609b8e7c6a946697d9c1bb2
Thomas Garayua <tgarayua@gmail.com>
2020-09-22 15:01:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def monopoly_with_third_tier
       :reading_railroad => {},
       :pennsylvania_railroad => {},
       :b_and_o_railroad => {},
-      
+      :shortline_railroad => {}
     }
   }
   # When you start writing the implementation for this method, copy the Hash
