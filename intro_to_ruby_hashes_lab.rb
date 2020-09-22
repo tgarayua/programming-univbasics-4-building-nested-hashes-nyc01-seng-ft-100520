aec08ea69524be61b8bf8d353cfe4234ec65e56a
Thomas Garayua <tgarayua@gmail.com>
2020-09-22 14:56:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def monopoly_with_second_tier
   end
 
 def monopoly_with_third_tier
-  :rent_in_dollars => {
+  return monopoly_with_second_tier :rent_in_dollars => {
     :one_piece_owned => 25,
     :two_pieces_owned => 50,
     :three_pieces_owned => 100,
